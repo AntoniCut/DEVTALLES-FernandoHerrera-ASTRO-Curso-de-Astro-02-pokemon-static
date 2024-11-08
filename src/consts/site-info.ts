a3467b9f6057e7c27aff8771a5073e29f0b81399
Antonio <antonicut@gmail.com>
2024-11-08 23:14:39 +0100

    Add - Fin de la sección 3 - 02-pokemon-static

---
+++ src/consts/site-info.ts
@@ -0,0 +1,11 @@
+//  ***************************************************************
+// **********  02-pokemon-static/src/const/site-info.ts  ********** 
+// **************************************************************** 
+
+
+export const siteInfo = {
+
+    title: 'Pokemons Static',
+    description: 'A static site with Pokemon data'
+
+}
