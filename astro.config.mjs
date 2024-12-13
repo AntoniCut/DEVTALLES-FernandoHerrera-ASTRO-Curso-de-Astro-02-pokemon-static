371ad57f77dfa1f28601b68f40afe292d5f4ce28
Antonio <antonicut@gmail.com>
2024-12-13 23:34:54 +0100

    Add - Inicio de la sección 4 - 02-pokemon-static

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,20 @@
+//  -------------------------------------------
+//  ----------  /02/pokemon-static/  ----------
+//  ----------  astro.config.mjs  -------------
+//  -------------------------------------------
+
+
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
+import icon from 'astro-icon';
+
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  // Asegúrate de definir el subdirectorio correcto en "base"
+  //site: 'https://devtalles.antonydev.tech/astro/02-pokemon-static',
+  //base: '/astro/02-pokemon-static/',  // <--- Este es el subdirectorio donde se encuentra tu proyecto -----  Descomentar para Producción  -----
+  integrations: [tailwind(), icon()]
+
 });
--- src/interfaces/pokemon-list.response.ts
+++ src/interfaces/pokemon-list.response.ts
@@ -1,3 +1,14 @@
+//  ----------------------------------------------------------------
+//  ----------  /02/pokemon-static/  -------------------------------
+//  ----------  /src/interfaces/pokemon-list.response.ts  ----------
+//  ----------------------------------------------------------------
+
+
+/*
+    Para crear la interfaz de usuario ...
+    Copiamos la Respuesta de la API => Paleta de Comandos => Paste JSON as Code
+*/
+
 export interface PokemonListResponse {
     count:    number;
     next:     string;
