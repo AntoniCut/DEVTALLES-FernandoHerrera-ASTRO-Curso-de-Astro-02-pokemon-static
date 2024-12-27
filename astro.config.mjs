97159c163952afe4e306d636969dc6b5d4764e1e
Antonio <antonicut@gmail.com>
2024-12-27 23:19:54 +0100

    Add - sección 4 - 02-pokemon-static Terminar LocalStorage

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,7 +12,8 @@ import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-  // Asegúrate de definir el subdirectorio correcto en "base"
+  //base: '/udemy/pokemon-static',
+    // Asegúrate de definir el subdirectorio correcto en "base"
   //site: 'https://devtalles.antonydev.tech/astro/02-pokemon-static',
   //base: '/astro/02-pokemon-static/',  // <--- Este es el subdirectorio donde se encuentra tu proyecto -----  Descomentar para Producción  -----
   integrations: [tailwind(), icon()]
