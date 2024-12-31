12e410d716065ebabd2caf1bd92cd2ca10eba60c
Antonio <antonicut@gmail.com>
2024-12-31 19:29:43 +0100

    Add - sección 4 - 02-pokemon-static Sección Terminada

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,14 +8,19 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import icon from 'astro-icon';
+import solidJs from '@astrojs/solid-js';
 
 
 // https://astro.build/config
 export default defineConfig({
-  //base: '/udemy/pokemon-static',
+
+    //base: '/udemy/pokemon-static',
     // Asegúrate de definir el subdirectorio correcto en "base"
-  //site: 'https://devtalles.antonydev.tech/astro/02-pokemon-static',
-  //base: '/astro/02-pokemon-static/',  // <--- Este es el subdirectorio donde se encuentra tu proyecto -----  Descomentar para Producción  -----
-  integrations: [tailwind(), icon()]
+    
+    //site: 'https://devtalles.antonydev.tech/astro/02-pokemon-static',
+    
+    //base: '/astro/02-pokemon-static/',  // <--- Este es el subdirectorio donde se encuentra tu proyecto -----  Descomentar para Producción  -----
+    
+    integrations: [tailwind(), icon(), solidJs()]
 
 });
